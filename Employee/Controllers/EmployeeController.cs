8fe4c31fcd147964570195a14694108a4da991bc
MDKAZEEMUDDIN <mohammedk@moback.com>
2022-11-04 18:50:42 +0530

    Add project files.

---
+++ Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employee.model;
+using System.Data.SqlClient;
+using System.Net.WebSockets;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Demo_Core_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public IConfiguration _configuration { get; }
+        SqlConnection con;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+             con = new SqlConnection(_configuration.GetConnectionString("Company"));
+
+        }
+
+        [HttpGet]
+        public JsonResult GetAllEmployee()
+        {
+            List<Emp_Model> employees = new List<Emp_Model>();
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Employee2";
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    employees.Add(new Emp_Model
+                    {
+                        Id = Convert.ToInt32(sdr["Id"]),
+                        EmployeeName = Convert.ToString(sdr["EmployeeName"]),
+                        Age = Convert.ToInt32(sdr["Age"])
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return new JsonResult(employees);
+
+        }
+
+        [HttpPost]
+        public Boolean AddEmployee(Emp_Model employee)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                //insert into Employee2(Id,EmployeeName,Age)values(2,'Kazeem',24);
+                cmd.CommandText = "Insert into Employee2(ID,EmployeeName, Age) values(" + employee.Id + ",'" + employee.EmployeeName + "'," + employee.Age + ")";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        [HttpPut]
+        public Boolean UpdateEmployee(int id, Emp_Model employee)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update Employee2 Set EmployeeName='" + employee.EmployeeName + "', Age=" + employee.Age + " Where(ID=" + id + ");";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        [HttpDelete]
+        public Boolean DeleteEmployee(int id)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Delete from Employee2 Where(ID=" + id + ");";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ Employee/model/Emp_Model.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee.model
+{
+    public class Emp_Model
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
